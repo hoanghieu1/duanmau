afc9248d300d9e04b3674456cd01e432bd75b1b7
Your Name <your@email.com>
2025-12-01 20:30:06 +0700

    viết HomeController cho client

--- controllers/client/HomeController.php
+++ controllers/client/HomeController.php
@@ -4,6 +4,15 @@ class HomeController
 {
     public function index() 
     {
+        // gọi model Product
+        $productModel = new Product();
+
+
+        // lấy 8 sản phẩm mới nhất
+        $newProducts = $productModel->getNewProducts();
+
+        $view = 'client/home';
+        $title = 'Trang chủ';
         require_once PATH_VIEW_CLIENT . 'main.php';
     }
 }
