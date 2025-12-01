b71aca6a714f182d035a0d55856062ecbd597fb3
Your Name <your@email.com>
2025-12-01 20:24:18 +0700

    viết routes cho client

--- routes/client.php
+++ routes/client.php
@@ -3,5 +3,15 @@
 $action = $_GET['action'] ?? '/';
 
 match ($action) {
+    // Trang chủ client
     '/'         => (new HomeController)->index(),
+    
+    // Danh sách sản phẩm theo danh mục
+    'products-by-category' => (new ProductController)->listByCategory(),
+
+    // Trang chi tiết sản phẩm
+    'product-detail' => (new ProductClientController)->detail(),
+
+    // DEFAULT
+    default     => (new HomeController)->index(),
 };
